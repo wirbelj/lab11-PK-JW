146041c3aaec44fd9f590e3943c88dade118bdeb
Payton Keller <pkeller@rfmunroe.org>
2025-11-17 16:51:46 -0500

    changed

--- calculator.py
+++ calculator.py
@@ -8,38 +8,41 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
 import math
 
-def add(a, b): 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
     return a + b
 
 def sub(a, b):
     return a - b
 
-def mul(a,b):
+def mul(a, b):
     return a * b
 
 def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    else:
-        return b / a
+    if b == 0:
+        raise ZeroDivisionError("Division by zero")
+    return a / b
 
 def log(a, b):
-    if b <= 0:
-        raise ValueError
-    else:
-        return math.log(a,b)
+    # conditions for valid log:
+    # a > 0
+    # b > 0 and b != 1
+    if a <= 0:
+        raise ValueError("Log argument must be > 0")
+    if b <= 0 or b == 1:
+        raise ValueError("Invalid logarithm base")
+    return math.log(a, b)
 
 def exp(a, b):
-    return math.exp(a,b)
-
-def square_root(a):
-    if a < 0:
-        raise ValueError
-    else:
-        return math.sqrt(a)
-
-def hypotenuse(a,b):
-    return math.hypot(a,b)
+    return a ** b
 
